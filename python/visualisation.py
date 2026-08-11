6067a0d670d4467a3328cad0bc03751ffcfdde74
raman <hd2506746@gmail.com>
2026-08-11 17:05:55 +0530

    add GA4 analysis outputs and visualization

---
+++ python/visualisation.py
@@ -0,0 +1,7 @@
+import pandas as pd 
+import matplotlib.pyplot as plt
+
+print("GA4 Visualization Started...")
+
+monthly_revenue_df = pd.read_csv("data/monthly_revenue.csv")
+print(monthly_revenue_df)
