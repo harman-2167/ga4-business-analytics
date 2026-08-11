faf45b7a7b40cfdffc2426ad36270cd19c23b620
raman <hd2506746@gmail.com>
2026-08-11 16:44:28 +0530

    update ga4_analysis.py

--- python/ga4_analysis.py
+++ python/ga4_analysis.py
@@ -24,3 +24,74 @@ kpi_df = client.query(query).to_dataframe()
 
 print("\n========== GA4 BUSINESS KPIs ==========")
 print(kpi_df)
+
+# ----------------------------------------------------------
+# 2. MONTHLY REVENUE TREND
+# ----------------------------------------------------------
+
+query = f"""
+SELECT 
+    FORMAT_DATE(
+        '%Y-%m',
+        PARSE_DATE('%Y%m%d', event_date)
+        ) AS month,
+    SUM(ecommerce.purchase_revenue_in_usd) AS monthly_revenue,
+    COUNTIF(event_name = 'purchase') AS monthly_purchases
+FROM `{TABLE}`
+WHERE event_name = 'purchase'
+GROUP BY month
+ORDER BY month
+"""
+
+monthly_revenue_df = client.query(query).to_dataframe()
+
+print("\n=========== MONTHLY REVENUE TREND ===========")
+print(monthly_revenue_df)
+
+# ----------------------------------------------------------
+# 3. TOP PRODUCTS BY REVENUE
+# ----------------------------------------------------------
+
+query = f"""
+SELECT
+    item.item_name AS product_name,
+    SUM(item.price_in_usd * item.quantity) AS revenue,
+    SUM(item.quantity) AS units_sold
+FROM `{TABLE}`,
+    UNNEST(items) AS item
+WHERE event_name = 'purchase'
+GROUP BY product_name
+ORDER BY revenue
+DESC
+LIMIT 20
+    """
+product_df = client.query(query).to_dataframe()
+
+print("\n============= TOP PRODUCTS BY REVENUE =============")
+print(product_df)
+
+# ------------------------------------------------------------
+# 4. ACQUISITION SOURCE PERFORMANCE
+# ------------------------------------------------------------
+
+query = f"""
+SELECT 
+    traffic_source.source AS source,
+    COUNT(DISTINCT user_pseudo_id) AS users,
+    COUNTIF(event_name = 'purchase') AS purchases,
+    SUM(
+        CASE
+        WHEN event_name = 'purchase'
+        THEN ecommerce.purchase_revenue_in_usd
+        ELSE 0
+        END
+    ) AS revenue
+FROM `{TABLE}`
+GROUP BY source 
+ORDER BY revenue
+DESC """
+
+source_df = client.query(query).to_dataframe()
+
+print("\n============= ACQUISITION SOURCE PERFORMANCE =============")
+print(source_df)
