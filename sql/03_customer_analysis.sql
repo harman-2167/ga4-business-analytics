b7e36419544d2aedaa135d69427a3d986ce996e9
raman <hd2506746@gmail.com>
2026-08-14 18:04:26 +0530

    update customer_analysis

--- sql/03_customer_analysis.sql
+++ sql/03_customer_analysis.sql
@@ -37,3 +37,18 @@ WHERE event_name = 'purchase'
 GROUP BY user_pseudo_id
 ORDER BY purchase_count DESC;
 
+---------------------------4. Purchase Frequency Distribution----------------
+
+SELECT
+    purchase_count,
+    COUNT(*) AS number_of_customers
+FROM (
+    SELECT
+        user_pseudo_id,
+        COUNT(*) AS purchase_count
+    FROM `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*`
+    WHERE event_name = 'purchase'
+    GROUP BY user_pseudo_id
+)
+GROUP BY purchase_count
+ORDER BY purchase_count;
