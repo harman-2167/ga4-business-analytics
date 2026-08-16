7896ac1fa37fc5de2049e99966d3163e32092636
raman <hd2506746@gmail.com>
2026-08-16 18:24:56 +0530

    UPDATE : funnel analysis sql by adding dropoff query

--- sql/04_funnel_analysis.sql
+++ sql/04_funnel_analysis.sql
@@ -52,3 +52,62 @@ SELECT
         AS checkout_to_purchase_rate
 
 FROM funnel;
+
+------------------------ 3. OVERALL PURCHASE CONVERSION RATE -------------------
+
+SELECT 
+    COUNT(DISTINCT user_pseudo_id) AS total_users,
+
+    COUNT(DISTINCT CASE 
+    WHEN event_name = 'purchase'
+    THEN user_pseudo_id
+    END) AS purchasing_users,
+
+    ROUND(COUNT(DISTINCT CASE 
+    WHEN event_name = 'purchase'
+    THEN user_pseudo_id
+    END) * 100 / COUNT(DISTINCT user_pseudo_id),2
+    ) AS overall_purchase_conversion_rate
+
+FROM `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*`; 
+
+----------------------- 4. FUNNEL DROP-OFF RATE ---------------------------
+
+WITH funnel AS (
+    SELECT 
+        COUNT(DISTINCT CASE 
+            WHEN event_name = 'view_item'
+            THEN user_pseudo_id
+            END) AS product_viewers,
+        COUNT(DISTINCT CASE 
+            WHEN event_name = 'add_to_cart'
+            THEN user_pseudo_id
+            END) AS cart_users,
+        COUNT(DISTINCT CASE 
+            WHEN event_name = 'begin_checkout'
+            THEN user_pseudo_id
+            END) AS checkout_users,
+        COUNT(DISTINCT CASE 
+            WHEN event_name = 'purchase'
+            THEN user_pseudo_id
+            END) AS purchasing_users,
+
+    FROM `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*`
+)
+
+SELECT 
+    product_viewers,
+    cart_users,
+    checkout_users,
+    purchasing_users
+
+    ROUND((product_viewers - cart_users)* 100 / product_viewers,2)
+    AS view_to_cart_dropoff_rate,
+
+    ROUND((cart_users - checkout_users) * 100 / cart_users , 2)
+    AS begin_checkout_dropoff_rate,
+
+    ROUND((checkout_usesrs - purchasing_users) * 100 / checkout_users ,2)
+    AS purchase_dropoff_rate
+
+FROM funnel;
